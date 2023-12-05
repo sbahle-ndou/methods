ddec7a9e56bf10c12c4bb2f3ca74111e30a33356
SBICZA01\A253563 <sbahle.ndou@standardbank.co.za>
2023-12-05 14:14:39 +0200

    Add project files.

---
+++ methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(70, "John", 15);
+            student1 student2 = new student1(40, "Tim", 10);
+
+            Console.WriteLine(student1.IsObese());
+            Console.WriteLine(student2.IsObese());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methods/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    internal class Student
+    {
+        public double weight;
+        public string name;
+        public int age;
+
+        public Student(double aWeight, string aNmae, int aAge)
+        {
+            this.weight = aWeight;
+            this.name = aNmae;
+            this.age = aAge;
+        }
+
+        public bool IsObese()
+        {
+            if (weight > 60)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+    }
+}
